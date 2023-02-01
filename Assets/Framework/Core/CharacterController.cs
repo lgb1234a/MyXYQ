7bc70539132e96e48c96540064c3608a70a41fdc
chenyn <chenynle@gmail.com>
2023-02-01 12:35:41 +0800

    优化角色跟随鼠标移动临近障碍物转向

--- Assets/Framework/Core/CharacterController.cs
+++ Assets/Framework/Core/CharacterController.cs
@@ -10,17 +10,13 @@ namespace Framework
         public float m_speed = 1.0f;
         public Animator m_animator;
         private Queue<Vector2> m_pathLocations = new Queue<Vector2>();
-        private Coroutine m_currentMove;
-        // Start is called before the first frame update
-        void Start()
-        {
-            StartCoroutine(HandleCharacterMove());
-        }
+        private Vector2 m_nextMoveLocation = Vector2.left;
 
         // Update is called once per frame
         void Update()
         {
             HandleMouseClicked();
+            HandleCharacterMove();
         }
 
         void HandleMouseClicked() {
@@ -30,17 +26,10 @@ namespace Framework
                     RaycastHit hitInfo;
                     if (Physics.Raycast(ray, out hitInfo, 200f)) {
                         var paths = LevelManager.Instance.Navigate(transform.position, hitInfo.point);
-                        StopMoveCoroutine();
+                        TriggerIdleAnimator();
                         EnqueuePathLocations(paths);
                     }
                 }
-            }
-        }
-
-        void StopMoveCoroutine() {
-            if (m_currentMove != null) {
-                StopCoroutine(m_currentMove);
-                m_currentMove = null;
             }
         }
 
@@ -52,59 +41,57 @@ namespace Framework
             }
         }
 
-        IEnumerator HandleCharacterMove() {
-            while (true)
-            {
-                if (GameManager.Instance.m_inAutoMoveState) {
-                    yield return HandleCharacterMoveFollowMousePosition();
-                }else if (m_pathLocations.Count > 0) {
-                    yield return HandleCharacterNavigateMove();
-                }else {
-                    yield return new WaitForEndOfFrame();
-                }
+        void HandleCharacterMove() {
+            if (GameManager.Instance.m_inAutoMoveState) {
+                HandleCharacterMoveFollowMousePosition();
+            }else {
+                HandleCharacterNavigateMove();
             }
         }
 
-        IEnumerator HandleCharacterMoveFollowMousePosition() {
+        void HandleCharacterMoveFollowMousePosition() {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo, 200f)) {
+                var movement = hitInfo.point - transform.position;
                 if (LevelManager.Instance.CanMove(transform, hitInfo.point)) {
-                    var movement = hitInfo.point - transform.position;
                     TranslateTransform(movement);
+                }else {
+                    TriggerRunAnimator(movement);
                 }
             }
-            yield return new WaitForEndOfFrame();
         }
 
-        IEnumerator HandleCharacterNavigateMove() {
-            var nextLocation = m_pathLocations.Dequeue();
-            var distance = Vector3.Distance(transform.position, new Vector3(nextLocation.x, nextLocation.y, 0));
-            var movement = new Vector3(nextLocation.x, nextLocation.y, 0) - transform.position;
-            m_currentMove = StartCoroutine(Move(movement));
-            var moveInterval = distance/m_speed;
-            yield return new WaitForSeconds(moveInterval);
-            StopMoveCoroutine();
-            if (m_pathLocations.Count == 0) {
-                // 上面计算的moveInterval可能无法跑满最后一帧就结束了，所以需要单独处理这部分偏移量
-                m_currentMove = StartCoroutine(FixEndLocationDeviation(nextLocation));
+        void HandleCharacterNavigateMove() {
+            if (m_pathLocations.Count > 0) {
+                if (IsNextMoveLocationInvalid()) {
+                    m_nextMoveLocation = m_pathLocations.Dequeue();
+                }
+                else if (IsArrived(m_nextMoveLocation)) {
+                    m_nextMoveLocation = m_pathLocations.Dequeue();
+                }
             }
+            if (IsNextMoveLocationInvalid())
+                return;
+            
+            if (IsArrived(m_nextMoveLocation)) {
+                InvalidNextMoveLocation();
+                TriggerIdleAnimator();
+                return;
+            }
+
+            var distance = Vector3.Distance(transform.position, new Vector3(m_nextMoveLocation.x, m_nextMoveLocation.y, 0));
+            var movement = new Vector3(m_nextMoveLocation.x, m_nextMoveLocation.y, 0) - transform.position;
+            TranslateTransform(movement);
         }
 
-        IEnumerator Move(Vector3 movement) {
-            while(true) {
-                TranslateTransform(movement);
-                yield return new WaitForEndOfFrame();
-            }
+        
+        bool IsNextMoveLocationInvalid() {
+            return m_nextMoveLocation == Vector2.left;
         }
 
-        IEnumerator FixEndLocationDeviation(Vector2 destination) {
-            while(!IsArrived(destination)) {
-                var movement = new Vector3(destination.x, destination.y, 0) - transform.position;
-                TranslateTransform(movement, false);
-                yield return new WaitForEndOfFrame();
-            }
-            TriggerIdleAnimator();
+        void InvalidNextMoveLocation() {
+            m_nextMoveLocation = Vector2.left;
         }
 
         bool IsArrived(Vector2 destination) {
